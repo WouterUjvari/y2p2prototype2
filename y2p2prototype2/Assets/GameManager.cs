1446c7d45f48282d14f936cb33d2e4e0969f20d8
GMD1A_WouterUjvari <wouter.ujvari@gmail.com>
2018-01-11 21:44:08 +0100

    desktopPost

---
+++ y2p2prototype2/Assets/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    //todd
+    public Animator animToddy;
+
+    //clickerstuff
+    public int iHowardPower;
+    public int iMetaCriticHealth;
+    public int iBudget;
+
+    
+
+
+
+
+    private void Start()
+    {
+        
+    }
+
+
+}

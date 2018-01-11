5ff325041fa335174b7eebffb38a999ffe7f0e67
GMD1A_WouterUjvari <wouter.ujvari@gmail.com>
2018-01-11 17:26:36 +0100

    post

---
+++ y2p2prototype2/Assets/DestroyThis.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyThis : MonoBehaviour {
+
+    public GameObject obj;
+    
+    public void DestroyThisBitch()
+    {
+        Destroy(obj);
+    }
+}
